d2575e0411db434833ca86e4c1af16bdc00e7432
santhoshsaravanamuthu <santhoshs.csbs2020@citchennai.net>
2024-10-04 15:52:33 +0530

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '31f870bc4815d413e6b733d650b2fd53';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
